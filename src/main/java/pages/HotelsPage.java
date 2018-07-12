653637fae19b04e443ad61dada3b9f71a474ab0e
Lipika <lipika@Ramyas-Air.lan>
2018-07-12 15:32:45 +1000

    minor changes

---
+++ src/main/java/pages/HotelsPage.java
@@ -0,0 +1,31 @@
+package pages;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+
+import java.util.Iterator;
+import java.util.List;
+
+
+public class HotelsPage extends BasePage {
+
+    @FindBy(xpath="//button[text()='Details']")
+    private List<WebElement>  hotelDetails;
+
+    public HotelsPage(WebDriver driver){
+        super(driver);
+    }
+
+
+    public HotelsDetailsPage clickHotelsDetails(){
+        Iterator<WebElement> hotelDetail = hotelDetails.iterator();
+        if(hotelDetail.hasNext()) {
+            hotelDetail.next().click();
+        }
+        return new HotelsDetailsPage(driver);
+    }
+
+
+}
